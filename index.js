629fca895d1a5c8e648626096a34fc2de7276e4a
rose534 <wambuirose534@gmail.com>
2022-12-11 12:20:30 +0300

    add

--- index.js
+++ index.js
@@ -6,10 +6,30 @@ fetch(API)
 
 function renderFilm(film) {
     const filmMenuDiv = document.getElementById("film-menu");
+    const titleDiv = document.getElementById("film-details"); 
+    const runDiv = document.getElementById("runtime")
+    const showDiv = document.getElementById("showtime")
+    const availableDiv = document.getElementById("Available-tickets")
 
     const filmPoster = document.createElement("img");
     filmPoster.src = film.poster;
     filmMenuDiv.append(filmPoster); 
+
+    const filmTitle = document.createElement("p");
+    filmTitle.textContent = film.title;
+    titleDiv.append(filmTitle);
+
+    const filmRuntime = document.createElement("p");
+    filmRuntime.textContent = film.runtime;
+    runDiv.append(filmRuntime);
+
+    const filmShowtime = document.createElement("p");
+    filmShowtime.textContent = film.showtime;
+    showDiv.append(filmShowtime);
+
+    const filmAvailability = document.createElement("p");
+    filmAvailability.textContent = (film.capacity - film.tickets_sold);
+    availableDiv.append(filmAvailability);
 
 }
 
