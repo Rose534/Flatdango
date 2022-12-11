dc10f2476a4043a42e5bcb565c79e2584917d1a4
rose534 <wambuirose534@gmail.com>
2022-12-11 14:18:12 +0300

    add

---
+++ to.js
@@ -0,0 +1,34 @@
+const API = "http://localhost:3000/films/1";
+
+fetch(API)
+.then((res) => res.json())
+.then(renderFilm);
+
+function renderFilm(film) {
+    const filmMenuDiv = document.getElementById("film-menu");
+    const titleDiv = document.getElementById("film-details"); 
+    const runDiv = document.getElementById("runtime")
+    const showDiv = document.getElementById("showtime")
+    const availableDiv = document.getElementById("Available-tickets")
+
+    const filmPoster = document.createElement("img");
+    filmPoster.src = film.poster;
+    filmMenuDiv.append(filmPoster); 
+
+    const filmTitle = document.createElement("p");
+    filmTitle.textContent = film.title;
+    titleDiv.append(filmTitle);
+
+    const filmRuntime = document.createElement("p");
+    filmRuntime.textContent = film.runtime;
+    runDiv.append(filmRuntime);
+
+    const filmShowtime = document.createElement("p");
+    filmShowtime.textContent = film.showtime;
+    showDiv.append(filmShowtime);
+
+    const filmAvailability = document.createElement("p");
+    filmAvailability.textContent = (film.capacity - film.tickets_sold);
+    availableDiv.append(filmAvailability);
+
+}
